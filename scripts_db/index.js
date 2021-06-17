4e4cb975b15e8df8bc935294b4afc27901304335
arthurwilson707 <85751689+arthurwilson707@users.noreply.github.com>
2021-06-16 23:28:20 -0300

    alteracoes_na_estrutura_de_dados

--- scripts_db/index.js
+++ scripts_db/index.js
@@ -4,7 +4,27 @@ const db = {
     users: [{
         name: "Cris",
         email: "cris@gmail.com",
-        password: "12345Cr!ss"
+        password: "12345Cr!ss",
+        user_type: "normal"
+    },
+    {
+        name: "admin",
+        email: "admin@admin.com",
+        password: "12345Admin!",
+        user_type: "admin"
+    }],
+
+    article: [{
+            id : 1,
+            author : "fulano",
+            data : "01/02/2021",
+            title : "Menu acessível",
+            category: "categoria1",
+            content :   '<p>\
+                            <h1>Title</h1>\
+                            lorem….\
+                            <img src="" />\
+                        </p>'
     }]
 }
 
