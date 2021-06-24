822b9e76f98982382c9aea2892a4d18e67b82621
arthurwilson707 <85751689+arthurwilson707@users.noreply.github.com>
2021-06-23 23:18:11 -0300

    bug fixes

--- scripts_db/index.js
+++ scripts_db/index.js
@@ -1,4 +1,5 @@
 const users = localStorage.getItem('users')
+const articles = localStorage.getItem('articles')
 
 const db = {
     users: [{
@@ -32,6 +33,10 @@ const db = {
 
 if (users) {
     db.users = JSON.parse(users)
+}
+
+if(articles) {
+    db.articles = JSON.parse(articles)
 }
 
 export { db }
